e1fa8a6376df301b6594ccaf9410ffcefbce8686
Emanuel <manny.cov09@gmail.com>
2023-09-06 16:36:20 -0700

    Complete count ouccrrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,9 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) count += 1;
+  }
+  return count;
+}
 
 module.exports = countOccurrences;
