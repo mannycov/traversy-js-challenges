d13a70c65d73f1444ac10f8393d158c8d91a1a1e
Emanuel <manny.cov09@gmail.com>
2023-09-06 16:40:24 -0700

    Complete findMaxNumber

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+  let maxNumber = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
