6af6be2f3e76b318e44e8ede6e1cce1355191c68
Emanuel <manny.cov09@gmail.com>
2023-09-21 16:41:00 -0700

    Complete reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    // append each character to the empty string
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
 
 module.exports = reverseString;
