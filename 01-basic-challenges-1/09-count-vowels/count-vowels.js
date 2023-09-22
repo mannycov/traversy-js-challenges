1288bfd414034564e27be9f0cf22f2d82ed58455
Emanuel <manny.cov09@gmail.com>
2023-09-21 18:41:46 -0700

    Complete count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+function countVowels(str) {
+  let vowelCount = 0;
+  const formattedStr = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (formattedStr[i] === "a") {
+      vowelCount += 1;
+    } else if (formattedStr[i] === "e") {
+      vowelCount += 1;
+    } else if (formattedStr[i] === "i") {
+      vowelCount += 1;
+    } else if (formattedStr[i] === "o") {
+      vowelCount += 1;
+    } else if (formattedStr[i] === "u") {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
+}
 
 module.exports = countVowels;
