a9bd344d7228eaf51297079a31d63e3ea5893afb
Emanuel <manny.cov09@gmail.com>
2023-09-22 19:02:10 -0700

    Complete count vowles and remove duplicates

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,18 +1,36 @@
+// function countVowels(str) {
+//   let vowelCount = 0;
+//   const formattedStr = str.toLowerCase();
+//   for (let i = 0; i < str.length; i++) {
+//     if (formattedStr[i] === "a") {
+//       vowelCount += 1;
+//     } else if (formattedStr[i] === "e") {
+//       vowelCount += 1;
+//     } else if (formattedStr[i] === "i") {
+//       vowelCount += 1;
+//     } else if (formattedStr[i] === "o") {
+//       vowelCount += 1;
+//     } else if (formattedStr[i] === "u") {
+//       vowelCount += 1;
+//     }
+//   }
+//   return vowelCount;
+// }
+
 function countVowels(str) {
   let vowelCount = 0;
-  const formattedStr = str.toLowerCase();
-  for (let i = 0; i < str.length; i++) {
-    if (formattedStr[i] === "a") {
-      vowelCount += 1;
-    } else if (formattedStr[i] === "e") {
-      vowelCount += 1;
-    } else if (formattedStr[i] === "i") {
-      vowelCount += 1;
-    } else if (formattedStr[i] === "o") {
-      vowelCount += 1;
-    } else if (formattedStr[i] === "u") {
-      vowelCount += 1;
-    }
+  let formattedStr = str.toLowerCase();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    let char = formattedStr[i];
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    )
+      vowelCount++;
   }
   return vowelCount;
 }
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,48 @@
-function removeDuplicates() {}
+// O(n^2)
+// function removeDuplicates(arr) {
+//   const uniqueChars = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let isDuplicate = false;
+//     for (let j = uniqueChars.length - 1; j >= 0; j--) {
+//       if (arr[i] === uniqueChars[j]) {
+//         isDuplicate = true;
+//         break;
+//       }
+//     }
+//     if (!isDuplicate) {
+//       uniqueChars.push(arr[i]);
+//     }
+//   }
+//   return uniqueChars;
+// }
+
+// O(n)
+// function removeDuplicates(arr) {
+//   const uniqueCharsSet = new Set();
+//   const uniqueChars = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     uniqueCharsSet.add(arr[i]);
+//   }
+
+//   for (const char of uniqueCharsSet) {
+//     uniqueChars.push(char);
+//   }
+//   return uniqueChars;
+// }
+
+// O(n)
+// function removeDuplicates(arr) {
+//   const uniqueChars = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueChars.includes(arr[i])) uniqueChars.push(arr[i]);
+//   }
+//   return uniqueChars;
+// }
+
+// O(n)
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;
